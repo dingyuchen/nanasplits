740d7d72e91da860047567239b451d5c60200ef9
Sam Ding <dingyuchen@meta.com>
2025-12-05 19:52:53 +0800

    test

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -6,11 +6,27 @@ const bot = new Bot(env.TELEGRAM_BOT_TOKEN).on("message", (context) => {
     return context.send(context.text);
   }
 });
+
+bot.on("my_chat_member", (context) => {
+  if (
+    context.oldChatMember.status === "left" &&
+    context.newChatMember.status === "member"
+  ) {
+    return context.reply("Welcome to the group!");
+  } else if (
+    context.oldChatMember.status === "member" &&
+    context.newChatMember.status === "left"
+  ) {
+    return context.reply("Goodbye!");
+  }
+});
+
 await bot.init();
 
 export const POST = async (req: Request) => {
   try {
     const update = await req.json();
+    console.log(update);
     await bot.updates.handleUpdate(update);
     return new Response("Ok");
   } catch (e) {
