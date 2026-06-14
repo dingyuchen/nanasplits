df4c46a7969a02c0711677fabf4ff856cfe001fd
Ding YuChen <hi@ding.gg>
2026-06-13 17:45:15 -0700

    test

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -43,7 +43,6 @@ function AppRoute() {
 		useState<TelegramStatus>("checking");
 	const [launchParams, setLaunchParams] = useState<LaunchParams | null>(null);
 	const [rawInitData, setRawInitData] = useState<string | undefined>(undefined);
-	console.log("rawInitdata", rawInitData);
 
 	useEffect(() => {
 		let cleanup: VoidFunction | undefined;
@@ -120,16 +119,19 @@ function AppRoute() {
 function SignInPanel({ initData }: { initData: string | undefined }) {
 	const { signIn } = useAuthActions();
 	const [msg, setMsg] = useState("Signing in...");
-	console.log("initData", initData);
 	useEffect(() => {
-		if (initData) {
-			void signIn("telegram", { initData }).catch((err) => {
-				if (err instanceof ConvexError) {
-					console.error("err signing in", err);
-					setMsg(err.message);
-				}
-			});
+		if (!initData) {
+			console.log("no signin");
+			return;
 		}
+
+		console.log("calling sign in initData", initData);
+		void signIn("telegram", { initData }).catch((err) => {
+			if (err instanceof ConvexError) {
+				console.error("err signing in", err);
+				setMsg(err.message);
+			}
+		});
 	}, [initData, signIn]);
 
 	return (
